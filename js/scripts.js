8d92307730e8a364d55060b8ca2d88dd53e45f07
VersaType <versatype@gmail.com>
2015-06-09 12:48:05 -0700

    Final Project (finish by thursday)

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+var userChoice;
+var compChoice;
+var randomNum;
+
+function playerRock()  {
+
+	userChoice = 1;  //  rock means 1 in this case
+	
+	randomNumber();
+}
+
+function playerPaper()  {
+
+	userChoice = 2;  //  paper means 2 in this case
+
+	randomNumber();
+}
+
+function playerScissors()  {
+
+	userChoice = 3;  //  scissors means 3 in this case
+
+	randomNumber();
+}
+
+function randomNumber() {
+	randomNum = Math.floor(Math.random()*100);
+
+	computerChoice();
+}
+
+function computerChoice() {
+
+	if (randomNum <= 33) {
+		compChoice = 1;  // Computer chooses Rock	
+
+	} else if (randomNum <= 66){
+
+		compChoice = 2;  // Computer chooses Paper
+	} else if  (randomNum <= 100) {
+
+		compChoice = 3;	 // Computer chooses Scissors
+	};
+
+	compare();
+}
+
+function compare()  {
+
+	if (userChoice === compChoice) {
+			alert("It is a tie!");
+			alert("Refresh to start again!");
+
+	} else if (userChoice === 1 && compChoice === 2) {
+			alert("You lose.");
+
+	} else if (userChoice === 1 && compChoice === 3){
+			alert("You win!");
+
+	} else if (userChoice === 2 && compChoice === 1){
+			alert("You win!");
+
+	} else if (userChoice === 2 && compChoice === 3){
+			alert("You lose.");
+		
+	} else if (userChoice === 3 && compChoice === 1){
+			alert("You lose.");
+		
+	} else if (userChoice === 3 && compChoice === 2){
+			alert("You win!");		
+	}		
+
+}
